3c80b41ab6b24076fa9ca995a64f8ff319cc62ea
Denis <denis_guitar@live.com.au>
2015-09-21 16:18:04 +1000

    Fixed Cerulean Evolution Stone House Descriptions

--- text/maps/cerulean_badge_house.asm
+++ text/maps/cerulean_badge_house.asm
@@ -1,12 +1,109 @@
-_CeruleanHouse2Text1:: ; 990c9 (26:50c9)
-	text "Evolution Stones"
-	line "can make certain"
-	cont "#MON evolve."
+_CeruleanHouse2Text_74e77:: ; 990c9 (26:50c9)
+    text "Evolution STONEs"
+    line "can make certain"
+    cont "#MON evolve."
 
-	para "They are rare so"
-	line "keep an eye out."
+    para "I see you have"
+    line "at least one."
 
-	para "Those Stones have"
-	line "mysterious power!"
-	done
+    para "Those STONEs have"
+    line "mysterious power!"
+    prompt
 
+_CeruleanHouse2Text_74e7c:: ; 9913a (26:513a)
+    text "Now then..."
+
+    para "Which of the 9"
+    line "Stones should I"
+    cont "describe?"
+    done
+
+_CeruleanHouse2Text_74e81:: ; 99170 (26:5170)
+IF DEF(_YELLOW)
+    text "Come visit me any-"
+ELSE
+    text "Come visit me any"
+ENDC
+    line "time you wish."
+    done
+
+_FireStoneText::
+    text "Evolves Fire"
+    line "type #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "VULPIX."
+    prompt
+
+_ThunderStoneText::
+    text "Evolves Thunder"
+    line "type #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "PIKACHU."
+    prompt	
+	
+_WaterStoneText::
+    text "Evolves Water"
+    line "type #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "SHELLDER."
+    prompt
+
+_LeafStoneText::
+    text "Evolves Grass"
+    line "type #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "GLOOM."
+    prompt
+
+_IceStoneText::
+    text "Evolves a"
+    line "special #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "EEVEE."
+    prompt
+
+_SteelStoneText::
+    text "Evolves a"
+    line "special #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "SCYTHER."
+    prompt
+
+_FairyStoneText::
+    text "Evolves a"
+    line "special #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "EEVEE."
+    prompt
+
+_SunStoneText::
+    text "Evolves a"
+    line "special #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "EEVEE."
+    prompt
+    
+_MoonStoneText::
+    text "Evolves a"
+    line "special #MON."
+    
+    para "Use it on"
+    line "#MON such as"
+    cont "NIDORINO."
+    prompt
