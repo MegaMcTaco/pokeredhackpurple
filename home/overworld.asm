83abc1487b6c6fdfcad58e1184025e00072f7073
Denis <denis_guitar@live.com.au>
2015-09-28 20:44:34 +1000

    Added Running Shoes

--- home/overworld.asm
+++ home/overworld.asm
@@ -277,7 +277,14 @@ OverworldLoopLessDelay::
 	bit 6,a ; jumping a ledge?
 	jr nz,.normalPlayerSpriteAdvancement
 	call BikeSpeedup ; if riding a bike and not jumping a ledge
+	jr .notRunning
 .normalPlayerSpriteAdvancement
+; Add running shoes
+	ld a, [hJoyHeld] ; Check what buttons are being pressed
+	and B_BUTTON ; Are you holding B?
+	jr z, .notRunning ; If you aren't holding B, skip ahead to step normally.
+	call BikeSpeedup ; Make you go faster if you were holding B
+.notRunning ; Normal code resumes here
 	call AdvancePlayerSprite
 	ld a,[wWalkCounter]
 	and a
